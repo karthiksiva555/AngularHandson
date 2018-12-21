11913bd267b06b8939993dc0ca8c4d6b1349260c
MEDGATE\snarisetty <siva.narisetty@cority.com>
2018-12-21 10:59:33 -0500

    warning and success components added

--- AngularHandson/src/app/servers/servers.component.ts
+++ AngularHandson/src/app/servers/servers.component.ts
@@ -2,8 +2,19 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
+  //selector:'[app-servers]',
+  //selector:'.app-servers',
   templateUrl: './servers.component.html',
+  // template:`
+  //   <app-server></app-server> 
+  //   <app-server></app-server>
+  // `,
   styleUrls: ['./servers.component.css']
+  // styles:[`
+  //   h3 {
+  //     color:red;
+  //   }
+  // `]
 })
 export class ServersComponent implements OnInit {
 
